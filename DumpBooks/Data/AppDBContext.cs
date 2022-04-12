bbd96b8d8d7268b1aa2d54cbad29105cbe19b5fc
Prashul Jain <jainprashul@gmail.com>
2022-04-12 10:38:51 +0530

    Added DB String Conenction & DB context

---
+++ DumpBooks/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using DumpBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DumpBooks.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        // for each table need to create DB set 
+        public DbSet<Category> Categories { get; set; }
+
+
+}
+}
