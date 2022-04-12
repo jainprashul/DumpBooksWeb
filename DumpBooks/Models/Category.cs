6125f8abdb7e9a449754f12c9c0c37a4c48d89e5
Prashul Jain <jainprashul@gmail.com>
2022-04-12 10:17:16 +0530

    Added Data Annotation for the Category Model

--- DumpBooks/Models/Category.cs
+++ DumpBooks/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace DumpBooks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DumpBooks.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
