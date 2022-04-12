468469e164a095b4f725b00d70dc94d12093016f
Prashul Jain <jainprashul@gmail.com>
2022-04-12 10:14:35 +0530

    Added Category Model

---
+++ DumpBooks/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace DumpBooks.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
