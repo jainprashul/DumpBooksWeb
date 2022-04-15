855408049ac86422b2f2f7d181869d33214bbfef
Prashul Jain <jainprashul@gmail.com>
2022-04-15 14:57:30 +0530

    Added Update & Delete CRUD

--- DumpBooks/Controllers/CategoryController.cs
+++ DumpBooks/Controllers/CategoryController.cs
@@ -27,8 +27,7 @@ namespace DumpBooks.Controllers
         // POST: Category/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category)
-        {
+        public IActionResult Create(Category category){
             try
             {
                 if(category.Name == category.DisplayOrder.ToString())
@@ -49,5 +48,85 @@ namespace DumpBooks.Controllers
                 return View();
             }
         }
+
+        // GET: Category/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if (category == null) {
+                return NotFound();
+            }
+            
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            try
+            {
+                if (category.Name == category.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("DisplayOrder", "Display Order must be different from Name");
+                }
+                if (ModelState.IsValid)
+                {
+                    _db.Categories.Update(category);
+                    _db.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(category);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var category = _db.Categories.Find(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+
+
+
     }
 }
